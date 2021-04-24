8f479e64d862551f76902ad3533a5d5800e75b7a
Victoria Truong <victoriataitruong@gmail.com>
2021-04-24 11:05:31 -0700

    deploy my app

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    return (
-        <div className="container">
-            <div className="navbar">
-                <nav>
-                    <div className="logo">
-                        <Link to="/"><i className="fas fa-users"></i></Link>
-                    </div>
+	return (
+		<div className="container">
+			<div className="navbar">
+				<nav>
+					<div className="logo">
+						<Link to="/">
+							<i className="fas fa-users" />
+						</Link>
+					</div>
 
-                    <ul className="links">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/add">add Product</Link></li>
-                        <li><Link to="/">login</Link></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    )
-}
+					<ul className="links">
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/add">Add Personnel</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	);
+};
 
-export default Navbar
+export default Navbar;
